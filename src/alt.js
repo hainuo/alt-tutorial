d1ecf6ba95317c61a2c23531a7136a17f5d19f55
hainuo <admin@hainuo.info>
2016-07-08 20:00:00 +0800

    add utils

--- src/alt.js
+++ src/alt.js
@@ -1,5 +1,6 @@
 var Alt = require('alt');
 var alt = new Alt();
-
-Alt.debug('alt',alt);
+var chromeDebug = require('alt-utils/lib/chromeDebug');
+Alt.debug('alt', alt);
+chromeDebug(alt);
 module.exports = alt;
